8f73a6e8b388ea5f8a52accde9b446b72fcefdd5
Chris Ward <chris.ward@servicenow.com>
2014-07-21 15:46:12 -0700

    added some additional events

--- js/app.js
+++ js/app.js
@@ -5,6 +5,17 @@ $(document).ready(function() {
   })
   .mouseleave(function() {
     $('.ryu-ready').hide();
-	$('.ryu-still').show();
-  });
-})
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    // play hadouken sound
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').show();
+    // animate hadouken to the right of the screen
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+   })
+});
