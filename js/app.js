7bd72c97c15d1d8ae3026170ab2ecd68cb45d3aa
Chris Ward <chris.ward@servicenow.com>
2014-07-24 21:10:41 -0700

    Strikes a cool pose when X button is pressed Also fixed some bugs

--- js/app.js
+++ js/app.js
@@ -1,30 +1,56 @@
 $(document).ready(function() {
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
+    $('.ryu-cool').hide();
     $('.ryu-ready').show();
   })
+
   .mouseleave(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
+    $('.ryu-cool').hide();
   })
+
   .mousedown(function() {
     playHadouken();
     $('.ryu-ready').hide();
+    $('.ryu-cool').hide();
+    $('.ryu-still').hide();
     $('.ryu-throwing').show();
- 	$('.hadouken').finish().show()
-  		.animate(
-    	{'left': '600px'},
-    	600,
-    	function() {
-      	$(this).hide();
-      	$(this).css('left', '-15px');
-    	});
+  	$('.hadouken').finish().show()
+  	   .animate(
+    	 {'left': '600px'},
+    	 600,
+    	 function() {
+      	 $(this).hide();
+      	 $(this).css('left', '-15px');
+    })
   })
+
   .mouseup(function() {
     $('.ryu-throwing').hide();
+    $('.ryu-cool').hide();
     $('.ryu-ready').show();
    })
-});
+
+  $(this).keydown(function(event) {
+    if ( event.which == 88 ) {
+      $('.ryu-still').hide();
+      $('.ryu-ready').hide();
+      $('.ryu-throwing').hide();
+      $('.ryu-cool').show();
+    }
+  })
+
+  $(this).keyup(function() {
+    if ( event.which == 88 ) {
+      $('.ryu-cool').hide();
+      $('.ryu-ready').hide();
+      $('.ryu-throwing').hide();
+      $('.ryu-still').show();
+    }
+  })
+}); 
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
